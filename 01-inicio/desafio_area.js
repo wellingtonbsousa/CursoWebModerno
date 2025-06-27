fe595a58bcfdd87cb2ca11870c1c0a73d2fd1781
Wellington Barbosa e Sousa <wellington.b.sousa2014@gmail.com>
2025-06-27 17:06:49 -0300

    01-Inicio

---
+++ 01-inicio/desafio_area.js
@@ -0,0 +1,7 @@
+const PI = 3.141592
+let raio = 10
+
+let area = PI * raio * raio
+
+let resultado = 'A área de um raio 10 é ' + area + ' m2'
+console.log(resultado)
---
+++ 01-inicio/desafio_troca.js
@@ -0,0 +1,7 @@
+let a = 7
+let b = 94
+console.log(a, b)
+let temp = a
+a = b
+b = temp
+console.log(a, b)
