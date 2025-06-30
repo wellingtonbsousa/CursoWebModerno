1aea9f2f17eac52a679e8cdd5dfc84557ac72d65
Wellington Barbosa e Sousa <wellington.b.sousa2014@gmail.com>
2025-06-30 17:46:03 -0300

    Estruturas de Controle

---
+++ 03-EstruturasControle/if2.js
@@ -0,0 +1,7 @@
+function teste1(num) {
+    if (num > 7)
+        console.log(num) // dentro do if (sem chaves => apenas uma sentença)
+        console.log('Final') // fora do if
+}
+teste1(6)
+teste1(8)
